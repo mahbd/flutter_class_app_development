0b8eeabc2febbf3faf289f4954f6dcc2cab5bc34
Mahmudul Alam <mahmudula2000@gmail.com>
2022-01-15 16:34:27 +0600

    Completed api 15-01-22

--- profile/api/lib/home_page.dart
+++ profile/api/lib/home_page.dart
@@ -13,23 +13,47 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String? temp;
+  String? cityName;
+
+  @override
+  void initState() {
+    super.initState();
+    getWeatherData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Weather App'),
+      ),
       body: Container(
-        constraints: BoxConstraints.expand(),
+        constraints: const BoxConstraints.expand(),
         alignment: Alignment.center,
-        child: Text('25 C'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text('Current Temp'),
+            Text(
+              "$temp C",
+              style: const TextStyle(fontSize: 50),
+            ),
+            Text('City name: $cityName'),
+          ],
+        ),
       ),
     );
   }
-}
 
-getWeatherData() async {
-  String url =
-      "https://api.openweathermap.org/data/2.5/weather?q=rangpur&appid=642b5e9a01b487adf2673534bf7f87ca&q=metric";
-  var response = await http.get(Uri.parse(url));
-  var data = jsonDecode(response.body);
-  print(data['main']['temp']);
+  getWeatherData() async {
+    String url =
+        "https://api.openweathermap.org/data/2.5/weather?q=rangpur&appid=642b5e9a01b487adf2673534bf7f87ca&units=metric";
+    var response = await http.get(Uri.parse(url));
+    var data = jsonDecode(response.body);
+    setState(() {
+      temp = data['main']['temp'].floor().toString();
+      cityName = data['name'];
+    });
+  }
 }
