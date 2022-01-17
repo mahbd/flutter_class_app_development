f3441ecabe8ceb2a72bd87d473f4a6eb90402fec
Mahmudul Alam <mahmudula2000@gmail.com>
2022-01-17 15:55:58 +0600

    Added news app

--- flutter_news_app/lib/home_page.dart
+++ flutter_news_app/lib/home_page.dart
@@ -5,6 +5,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List category = ['Economic', 'Social', 'Political', 'Cultural', 'Sports'];
     return Scaffold(
         appBar: AppBar(
           title: const Text('Home'),
