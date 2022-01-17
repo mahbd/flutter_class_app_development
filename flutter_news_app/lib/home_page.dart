c4dfdcf76e3fe896b2e3f086d85b0d34f64f69a9
Mahmudul Alam <mahmudula2000@gmail.com>
2022-01-17 16:27:16 +0600

    Complete class

--- flutter_news_app/lib/home_page.dart
+++ flutter_news_app/lib/home_page.dart
@@ -8,21 +8,57 @@ class HomePage extends StatelessWidget {
     List category = ['Economic', 'Social', 'Political', 'Cultural', 'Sports'];
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Home'),
+          title: const Text('Hi'),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text("Latest news"),
-            const TextField(),
-            Container(
-              child: ListView.builder(
+            const Padding(
+              padding: EdgeInsets.only(top: 25, bottom: 16),
+              child: Text(
+                "Latest news",
+                style: TextStyle(fontSize: 25, color: Colors.deepPurple),
+              ),
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                suffixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+                fillColor: Colors.grey,
+                filled: true,
+              ),
+            ),
+            SizedBox(
+              height: 35,
+              child: ListView.separated(
+                  separatorBuilder: (context, index) {
+                    return const SizedBox(width: 16);
+                  },
                   scrollDirection: Axis.horizontal,
-                  itemCount: 5,
+                  itemCount: category.length,
                   itemBuilder: (context, index) {
                     return MaterialButton(
-                        onPressed: () {}, child: Text("Button $index"));
+                        minWidth: 80,
+                        shape: const StadiumBorder(),
+                        color: Colors.deepPurple,
+                        onPressed: () {},
+                        child: Text(
+                          category[index],
+                          style: const TextStyle(color: Colors.white),
+                        ));
                   }),
-            )
+            ),
+            Expanded(child: ListView.builder(itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  leading: Image.asset('assets/images/logo.png'),
+                  title: const Text(
+                    'This is an example of news heading',
+                  ),
+                  subtitle: const Text('Author: Jack and Rose'),
+                ),
+              );
+            }))
           ],
         ));
   }
--- flutter_news_app/lib/main.dart
+++ flutter_news_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/home_page.dart';
 import 'splash_screen.dart';
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashScreen(),
+      home: HomePage(),
     );
   }
 }
